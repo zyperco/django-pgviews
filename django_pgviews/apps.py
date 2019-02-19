56f24b467c891e4a3f7c839b52cac023faed130d
ed <ed@zyper.com>
2019-02-19 17:55:57 +0000

    Skip pgview sync if no migrations run

--- django_pgviews/apps.py
+++ django_pgviews/apps.py
@@ -11,22 +11,28 @@ class ViewConfig(apps.AppConfig):
     post_migrate signal handlers.
     """
     counter = 0
+    migrations_ran = False
     name = 'django_pgviews'
     verbose_name = 'Django Postgres Views'
 
     def sync_pgviews(self, sender, app_config, **kwargs):
         """Forcibly sync the views.
         """
+        if len(kwargs['plan']) > 0:
+            self.migrations_ran = True
         self.counter = self.counter + 1
         total = len([a for a in apps.apps.get_app_configs() if a.models_module is not None])
-        
+
         if self.counter == total:
+            if not migrations_ran:
+                log.info('No migrations run - skipping sync')
+                return
             log.info('All applications have migrated, time to sync')
             # Import here otherwise Django doesn't start properly
             # (models in app init are not allowed)
             from .models import ViewSyncer
             vs = ViewSyncer()
-            vs.run(force=True, update=True) 
+            vs.run(force=True, update=True)
 
     def ready(self):
         """Find and setup the apps to set the post_migrate hooks for.
